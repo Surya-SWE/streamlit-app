a4aa269bc7c8ddf2a8df69722a0641ad457958c8
Surya Potnuru <suryapotnuru2023@gmail.com>
2025-09-19 13:04:17 +0530

    chat assistant medical demo file pushed

--- main.py
+++ main.py
@@ -1,18 +1,48 @@
 import streamlit as st
+import random
+import time
 
-st.title("Hello Streamlit-er 👋")
+st.write("Streamlit loves LLMs!, [We can build our own chatbot app](<hyperlink>) in minutes.. then make it powerful by adding different widgets...")
 
-st.markdown(
-    """ 
-    This is a playground for you to try Streamlit and have fun. 
+st.caption("Note that this is only demo app not actually connected with LLMs")
 
-    **There's :rainbow[so much] you can build!**
-    
-    We prepared a few examples for you to get started. Just 
-    click on the buttons above and discover what you can do 
-    with Streamlit. 
-    """
-)
+# initally we need to initalie chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role" : "assistant", "content": "Let's start chatting!.."}]
 
-if st.button("Send balloons!"):
-    st.balloons()
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# accept user input
+if prompt := st.chat_input("What is up?"):
+    # then we need to add this into our chat history
+    st.session_state.messages.append({"role" : "user", "content" : prompt})
+
+    # now display this on chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # for the current prompt we need to add assistant response as well right..
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        assistant_response = random.choice(
+            [
+                "Hello there! How can I assist you today?",
+                "Do you need help?",
+                "Remember to drink plenty of water throughout the day to stay hydrated.",
+                "Schedule your annual check-up to keep track of your overall health.",
+                "Regular exercise helps improve both physical and mental health."
+            ]
+        )
+
+        # now simulate stream of response with miliiseconds delay
+        for chunk in assistant_response.split():
+            full_response += chunk + " "
+            time.sleep(0.02)
+            # add a blinking cursor to simulate typing
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role" : "assistant", "content" : full_response})
