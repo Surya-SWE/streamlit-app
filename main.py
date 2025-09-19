2e0a13452cc27a01bc1d10f25088292ee3878b5e
Surya Potnuru <suryapotnuru2023@gmail.com>
2025-09-19 13:08:42 +0530

    removed chat history

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ if "messages" not in st.session_state:
     st.session_state.messages = [{"role" : "assistant", "content": "Let's start chatting!.."}]
 
 # Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+# for message in st.session_state.messages:
+#     with st.chat_message(message["role"]):
+#         st.markdown(message["content"])
 
 # accept user input
 if prompt := st.chat_input("What is up?"):
